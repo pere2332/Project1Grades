ae2ca4811562cb41ac67a51cbc8a96bb506a9d29
Isidro <iperezzepeda@csumb.edu>
2020-09-06 17:49:20 -0700

    update2

--- app/src/main/java/com/example/project1grades/DB/AppDatabase.java
+++ app/src/main/java/com/example/project1grades/DB/AppDatabase.java
@@ -2,15 +2,18 @@ package com.example.project1grades.DB;
 
 
 import androidx.room.Database;
+import androidx.room.RoomDatabase;
 
 import com.example.project1grades.userlog;
 
 @Database(entities = {userlog.class}, version = 1)
-public abstract class AppDatabase {
+public abstract class AppDatabase extends RoomDatabase {
+
+    //User
     public static final String DBNAME = "db-userlog";
-
     public static final String USERLOG_TABLE = "userlog";
+    public abstract DAO getUserlogDAO();
 
-    public abstract UserLogDAO getUserlogDAO();
+
 
 }
--- app/src/main/java/com/example/project1grades/MainActivity.java
+++ app/src/main/java/com/example/project1grades/MainActivity.java
@@ -2,13 +2,35 @@ package com.example.project1grades;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button login;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        login = (Button) findViewById(R.id.LOGIN);
+
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Loginpage();
+            }
+        });
+    }
+
+    private void Loginpage(){
+        Intent intent = new Intent(this, LoginPage.class);
+        startActivity(intent);
     }
 }
