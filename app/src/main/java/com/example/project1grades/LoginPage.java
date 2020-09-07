6572a0fbd6681b69a2e1be59656f86a8011f55a1
Isidro <iperezzepeda@csumb.edu>
2020-09-06 17:42:44 -0700

    update3

--- app/src/main/java/com/example/project1grades/LoginPage.java
+++ app/src/main/java/com/example/project1grades/LoginPage.java
@@ -2,20 +2,19 @@ package com.example.project1grades;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.room.Room;
-import androidx.room.RoomDatabase;
 
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 import com.example.project1grades.DB.AppDatabase;
-import com.example.project1grades.DB.UserLogDAO;
+import com.example.project1grades.DB.DAO;
 
 import java.util.List;
 
 public class LoginPage extends AppCompatActivity {
 
-    UserLogDAO check;
+    DAO check;
     List<userlog> checku;
 
     Button login;
@@ -27,10 +26,11 @@ public class LoginPage extends AppCompatActivity {
 
         login = (Button) findViewById(R.id.signin);
 
-        check = (UserLogDAO) Room.databaseBuilder(this, AppDatabase.class, AppDatabase.DBNAME)
+        check = Room.databaseBuilder(this, AppDatabase.class, AppDatabase.DBNAME)
+                .allowMainThreadQueries()
                 .build()
-                .getUserlogDAO()
-                .getAllUserLogs();
+                .getUserlogDAO();
+
 
 
         login.setOnClickListener(new View.OnClickListener() {
