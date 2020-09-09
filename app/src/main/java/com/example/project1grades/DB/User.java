03bd4a4646178406eb921ffceac7bf3e4021897c
ChrJimenez <47259794+christianJimenez1999@users.noreply.github.com>
2020-09-08 17:12:47 -0700

    fixed log in and create issues. I think

---
+++ app/src/main/java/com/example/project1grades/DB/User.java
@@ -0,0 +1,46 @@
+package com.example.project1grades.DB;
+
+import androidx.room.Entity;
+import androidx.room.Ignore;
+import androidx.room.PrimaryKey;
+
+@Entity
+public class User {
+    @PrimaryKey(autoGenerate = true)
+    private long id;
+
+    private String username;
+    private String password;
+
+    public User(){}
+
+    @Ignore
+    public User(String username, String password){
+        this.username = username;
+        this.password = password;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
